caebe9b27e2e244503276f154cc3385950562e86
Zohaib Ul Hassan <121067014+zohaibulhassan02@users.noreply.github.com>
2024-07-31 15:09:24 +0500

    frontend URL Updated

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://mern-multi-vendor-store-frontend.vercel.app/",
     credentials: true,
   })
 );
