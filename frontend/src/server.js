4315c8333ef610811253e3a6ade2a85a9af8cd11
Zohaib Ul Hassan <121067014+zohaibulhassan02@users.noreply.github.com>
2024-07-31 14:55:09 +0500

    Server URL's Updated

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,8 +1,5 @@
 // export const server = "https://eshop-tutorial.vercel.app/api/v2";
 
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://mern-multi-vendor-store.vercel.app/api/v2";
 
-export const backend_url = "http://locahost:8000/"
-
-
-
+export const backend_url = "https://mern-multi-vendor-store.vercel.app/";
